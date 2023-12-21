f9e56c5f0300d99f3af08ae5b202b69c54369136
Marc <marc.ferbos@gmail.com>
2023-12-21 15:23:20 +0100

    Import logement + affichage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import './App.scss';
 import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
 import Appartement from './Components/Appartement';
+import Data from './Data/logement.json';
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <Banner></Banner>
-      <Appartement></Appartement>
+      {Data.map((logement, index) => {
+        return (
+          <Appartement
+            key={index}
+            id={logement.id}
+            title={logement.title}
+            cover={logement.cover}
+            location={logement.location}
+            tags={logement.tags}
+            description={logement.description}
+            host={logement.host}
+            rating={logement.rating}
+          ></Appartement>
+        );
+      })}
     </div>
   );
 }
--- src/Components/Appartement.js
+++ src/Components/Appartement.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Appartement.scss';
+import { Link } from 'react-router-dom';
 
 
-function Appartement() {
-    return (    
-    <div className = "Appartement" >
+function Appartement({ id, title, cover, location, tags, description, host, rating}) {
+    return ( 
+    <Link to={`/logement/${id}`} className="Appartement-link">   
+    <article className = "Appartement" >
+        <h3>{title}</h3>
+        <img src={cover} alt={title} />
 
-    </div>
+    </article>
+    </Link>
     );
 }
 
