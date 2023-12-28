8d03326d60a4237862f3f9c898e4789dfd007b59
Marc <marc.ferbos@gmail.com>
2023-12-28 02:22:03 +0100

    Page À Propos footer + navbar + banner

--- src/pages/Info/Info.js
+++ src/pages/Info/Info.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './Info.scss';
 
 import Navbar from '../../Components/Navbar';
+import Banner2 from '../../Components/Banner2';
+import Footer from '../../Components/Footer';
 
 function Info() {
     return (
+      <>
       <Navbar></Navbar>
+      <Banner2></Banner2>
+      <Footer></Footer>
+      </>
     );
   }
   
