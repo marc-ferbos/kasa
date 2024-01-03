0aa3f8f4d15f8bd198434f7037173a15e301d241
Marc <marc.ferbos@gmail.com>
2024-01-03 18:08:49 +0100

    Description animation + affichage logement

---
+++ src/Components/Collapse.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useState } from 'react';
+import './Collapse.scss';
+
+function Collapse({title, description}) {
+
+    const [isContentVisible, setIsContentVisible] = useState(false);
+    const ContentON = () => {
+        setIsContentVisible(!isContentVisible);
+    }
+    
+    return (
+        <div className="appartement-details">
+                <div className="description1">
+                    <span>{title}</span>
+                    <i className="fa-solid fa-chevron-up" onClick={ContentON}></i>
+                </div>
+                {isContentVisible && <p className="description-content">
+                    {description}
+                </p>}
+            </div>
+    )
+
+}
+
+export default Collapse;
--- src/pages/Info/Apartmentpage.js
+++ src/pages/Info/Apartmentpage.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './Apartmentpage.scss';
-import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Navbar from '../../Components/Navbar';
 import Footer from '../../Components/Footer';
+import Data from '../../Data/logement.json';
 
-function Apartmentpage({ id, title, cover, location, tags, description, host, rating}) {
+function Apartmentpage() {
+    const id = useParams().id; /* Récupère l'id de l'appartement dans l'URL */
+    let appartement = {};
+    Data.forEach((logement) => { /* Pour chaque élément de Data */
+        if (logement.id === id) { /* Si l'id de l'appartement correspond à l'id dans l'URL */
+            appartement = logement; /* On récupère l'appartement */
+        }
+    });
+    
+    let { title, cover, location, tags, description, host, rating } = appartement;
+
     return (
         <>
-        <Navbar></Navbar>
-        
-        <Link to={`/logement/${id}`} className="Appartement-lin">
+        <Navbar></Navbar>        
             <div className="Appartement">
             <img src={cover} alt={title} />
                 <div className="Appartement-info">
@@ -20,7 +29,7 @@ function Apartmentpage({ id, title, cover, location, tags, description, host, ra
                     <div className="Appartement-tags">
                     {Array.isArray(tags) && tags.length > 0 ? ( /* Vérifie si tags est un tableau et s'il contient au moins un élément */
                         tags.map((tag, index) => ( /* Pour chaque élément du tableau, on affiche un span avec le tag */
-                            <span key={index}>{tag}</span>
+                            <span key={index}>{tag}</span> /* key={index} permet d'identifier chaque élément du tableau */
                             ))
                             ) : (
                                 <span>No tags available</span> /* Si tags n'est pas un tableau ou s'il est vide, on affiche un message */
@@ -36,15 +45,13 @@ function Apartmentpage({ id, title, cover, location, tags, description, host, ra
                             {Array(rating) /* Crée un tableau de rating éléments */
                                 .fill() /* Remplit le tableau avec des valeurs vides */
                                 .map((_, i) => ( /* Pour chaque élément du tableau, on affiche une étoile */
-                                    <i key={i} className="fas fa-star"></i>
+                                    <i key={i} className="fas fa-star"></i> /* key={i} permet d'identifier chaque élément du tableau */
                                 ))}
                         </span>
                         <span className="Appartement-rating-reviews"> {host?.reviews} avis</span>
                     </div>
                 </div>
-                <img src={cover} alt={title} />
             </div>
-        </Link>
         <Footer></Footer>
         </>
     );
